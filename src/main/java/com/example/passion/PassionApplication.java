655cda211018f7ff71f5b1fe5a65854423e984b2
Yg-Hong <lion0077v@gmail.com>
2023-01-08 20:10:22 +0900

    [add] 스프링 프로젝트 생성

---
+++ src/main/java/com/example/passion/PassionApplication.java
@@ -0,0 +1,13 @@
+package com.example.passion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassionApplication.class, args);
+    }
+
+}
